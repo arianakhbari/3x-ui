2bcf402a975055be63128c95460ba7b32e61bf02
arianakhbari <77466277+arianakhbari@users.noreply.github.com>
2024-10-02 12:30:54 -0700

    outbound.go

--- web/service/outbound.go
+++ web/service/outbound.go
@@ -1,100 +1,91 @@
 package service
 
 import (
-	"x-ui/database"
-	"x-ui/database/model"
-	"x-ui/logger"
-	"x-ui/xray"
+    "x-ui/database"
+    "x-ui/database/model"
+    "x-ui/logger"
+    "x-ui/xray"
 
-	"gorm.io/gorm"
+    "gorm.io/gorm"
 )
 
 type OutboundService struct{}
 
-func (s *OutboundService) AddTraffic(traffics []*xray.Traffic, clientTraffics []*xray.ClientTraffic) (error, bool) {
-	var err error
-	db := database.GetDB()
-	tx := db.Begin()
+func (s *OutboundService) AddTraffic(traffics []*xray.Traffic, clientTraffics []*xray.ClientTraffic) error {
+    var err error
+    db := database.GetDB()
+    tx := db.Begin()
 
-	defer func() {
-		if err != nil {
-			tx.Rollback()
-		} else {
-			tx.Commit()
-		}
-	}()
+    defer func() {
+        if err != nil {
+            tx.Rollback()
+        } else {
+            tx.Commit()
+        }
+    }()
 
-	err = s.addOutboundTraffic(tx, traffics)
-	if err != nil {
-		return err, false
-	}
+    err = s.addOutboundTraffic(tx, traffics)
+    if err != nil {
+        return err
+    }
 
-	return nil, false
+    // If needed, process clientTraffics here
+
+    return nil
 }
 
 func (s *OutboundService) addOutboundTraffic(tx *gorm.DB, traffics []*xray.Traffic) error {
-	if len(traffics) == 0 {
-		return nil
-	}
+    if len(traffics) == 0 {
+        return nil
+    }
 
-	var err error
-
-	for _, traffic := range traffics {
-		if traffic.IsOutbound {
-
-			var outbound model.OutboundTraffics
-
-			err = tx.Model(&model.OutboundTraffics{}).Where("tag = ?", traffic.Tag).
-				FirstOrCreate(&outbound).Error
-			if err != nil {
-				return err
-			}
-
-			outbound.Tag = traffic.Tag
-			outbound.Up = outbound.Up + traffic.Up
-			outbound.Down = outbound.Down + traffic.Down
-			outbound.Total = outbound.Up + outbound.Down
-
-			err = tx.Save(&outbound).Error
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
+    for _, traffic := range traffics {
+        if traffic.IsOutbound {
+            err := tx.Model(&model.OutboundTraffics{}).
+                Where("tag = ?", traffic.Tag).
+                Updates(map[string]interface{}{
+                    "tag":   traffic.Tag,
+                    "up":    gorm.Expr("up + ?", traffic.Up),
+                    "down":  gorm.Expr("down + ?", traffic.Down),
+                    "total": gorm.Expr("total + ? + ?", traffic.Up, traffic.Down),
+                }).Error
+            if err != nil {
+                logger.Error("Failed to update outbound traffic: ", err)
+                return err
+            }
+        }
+    }
+    return nil
 }
 
 func (s *OutboundService) GetOutboundsTraffic() ([]*model.OutboundTraffics, error) {
-	db := database.GetDB()
-	var traffics []*model.OutboundTraffics
+    db := database.GetDB()
+    var traffics []*model.OutboundTraffics
 
-	err := db.Model(model.OutboundTraffics{}).Find(&traffics).Error
-	if err != nil {
-		logger.Warning("Error retrieving OutboundTraffics: ", err)
-		return nil, err
-	}
+    err := db.Model(&model.OutboundTraffics{}).Find(&traffics).Error
+    if err != nil {
+        logger.Warning("Error retrieving OutboundTraffics: ", err)
+        return nil, err
+    }
 
-	return traffics, nil
+    return traffics, nil
 }
 
 func (s *OutboundService) ResetOutboundTraffic(tag string) error {
-	db := database.GetDB()
+    db := database.GetDB()
+    var err error
 
-	whereText := "tag "
-	if tag == "-alltags-" {
-		whereText += " <> ?"
-	} else {
-		whereText += " = ?"
-	}
-
-	result := db.Model(model.OutboundTraffics{}).
-		Where(whereText, tag).
-		Updates(map[string]interface{}{"up": 0, "down": 0, "total": 0})
-
-	err := result.Error
-	if err != nil {
-		return err
-	}
-
-	return nil
+    if tag == "-alltags-" {
+        err = db.Model(&model.OutboundTraffics{}).
+            Updates(map[string]interface{}{"up": 0, "down": 0, "total": 0}).Error
+    } else {
+        err = db.Model(&model.OutboundTraffics{}).
+            Where("tag = ?", tag).
+            Updates(map[string]interface{}{"up": 0, "down": 0, "total": 0}).Error
+    }
+    if err != nil {
+        logger.Error("Failed to reset outbound traffic: ", err)
+        return err
+    }
+    return nil
 }
